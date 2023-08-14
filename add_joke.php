7be4ca13c963b0f820b4cfb8300211cb6850ffaa
ColeDNate <colednate@gmail.com>
2023-08-14 01:46:52 -0700

    Update add_joke.php
    change mysqli to pdo connection

--- add_joke.php
+++ add_joke.php
@@ -16,8 +16,7 @@ $userid = $_SESSION['userid'];
 echo "<h2>Trying to add a new joke " . $new_joke_question . " and " . $new_joke_answer . "</h2>";
 echo "<h2>For user " . $userid . "</h2>";
 
-$stmt = $mysqli->prepare("INSERT INTO jokes_table (JokeID, Joke_question, Joke_answer, user_id) VALUES (null, ?, ?, ?)");
-$stmt->bind_param("ssi", $new_joke_question, $new_joke_answer, $userid);
+$stmt = $conn->prepare("INSERT INTO jokes_table (JokeID, Joke_question, Joke_answer, user_id) VALUES VALUES (null, '$new_joke_question', '$new_joke_answer', '$userid')");
 
 $stmt->execute();
 $stmt->close();
