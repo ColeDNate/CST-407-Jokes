de160df066e009bad6397204ba392c56d32a89a0
ColeDNate <colednate@gmail.com>
2023-08-13 23:28:16 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,9 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
+    $privateEndpointAddress = "JokesAccess.default.CST407Network.privatelink.database.windows.net";
+    $port = "1433";
+    
     $conn = new PDO("sqlsrv:server = tcp:cst407jokes.database.windows.net,1433; Database = cst407jokes", "JokesIsPass123!", "Jokes123!");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
