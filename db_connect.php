6e6e5760ac85397e3ddb1e701b6d300d0a8d2ae9
ColeDNate <colednate@gmail.com>
2023-08-14 00:37:43 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,24 +3,18 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$privateEndpointAddress = "JokesAccess.default.CST407Network.privatelink.database.windows.net";
-$port = "1433";
-$username = "JokesIsPass123!";
-$password = "Jokes123!";
-$database_in_use = "cst407jokes";
+// modify these settings according to the account on your database server.
+$host = "localhost";
+$port = "3306";
+$username = "root";
+$user_pass = "root";
+$database_in_use = "jokes";
 
-try {
-    // PDO connection
-    $conn = new PDO("sqlsrv:server = tcp:$privateEndpointAddress,$port; Database = $database_in_use", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // MySQLi connection
-    $host = $privateEndpointAddress;
-    $user_pass = $password;
-    $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
-    
-    echo "Connected to Azure SQL Database successfully<br>";
-} catch (PDOException $e) {
-    echo "Failed to connect to Azure SQL Database: " . $e->getMessage();
+$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+if ($mysqli->connect_error) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
+echo $mysqli->host_info . "<br>";
+
 ?>
